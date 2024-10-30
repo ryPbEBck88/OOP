9b52b2823a314641852915bafeeb8209ebbdd743
Alexandr Khalupa <huskar0302@gmail.com>
2024-10-30 19:56:16 +0300

    Добавил проверок assert.

--- 3.3 practice 'creating a class and its methods'/3.py
+++ 3.3 practice 'creating a class and its methods'/3.py
@@ -59,3 +59,21 @@ class Numbers:
             list[int]: Список всех нулей из списка self.numbers.
         """
         return [i for i in self.numbers if i == 0]
+
+
+n1 = Numbers(1, 2, 3, 4, 5)
+assert n1.numbers == [1, 2, 3, 4, 5]
+n1.add_number(6)
+assert n1.numbers == [1, 2, 3, 4, 5, 6]
+assert n1.get_positive() == [1, 2, 3, 4, 5, 6]
+assert n1.get_negative() == []
+assert n1.get_zeroes() == []
+
+n2 = Numbers(-1, 0, 12, -15, 11, 0)
+assert n2.numbers == [-1, 0, 12, -15, 11, 0]
+n2.add_number(0)
+assert n2.get_positive() == [12, 11]
+assert n2.get_zeroes() == [0, 0, 0]
+assert n2.get_negative() == [-1, -15]
+
+print("Good")
